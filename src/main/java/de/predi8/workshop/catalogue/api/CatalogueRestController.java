7f61ba9ec62a67dc1fe2121dbece34a587fae8ec
oweiner <olaf.weiner@online.de>
2018-06-12 17:32:02 +0200

    Schulung Microservices: Restcontroller / Kafka

--- src/main/java/de/predi8/workshop/catalogue/api/CatalogueRestController.java
+++ src/main/java/de/predi8/workshop/catalogue/api/CatalogueRestController.java
@@ -5,20 +5,15 @@ import de.predi8.workshop.catalogue.domain.Article;
 import de.predi8.workshop.catalogue.error.NotFoundException;
 import de.predi8.workshop.catalogue.event.Operation;
 import de.predi8.workshop.catalogue.repository.ArticleRepository;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
@@ -30,10 +25,14 @@ import java.util.concurrent.TimeoutException;
 public class CatalogueRestController {
 	private ArticleRepository articleRepository;
 	private KafkaTemplate<String, Operation> kafka;
+	private ObjectMapper mapper;
 
-	public CatalogueRestController(ArticleRepository articleRepository, KafkaTemplate<String, Operation> kafka) {
+	public CatalogueRestController(ArticleRepository articleRepository,
+								   KafkaTemplate<String, Operation> kafka,
+								   ObjectMapper mapper) {
 		this.articleRepository = articleRepository;
 		this.kafka = kafka;
+		this.mapper = mapper;
 	}
 
 	@GetMapping
@@ -51,6 +50,61 @@ public class CatalogueRestController {
 
 		return article;
 	}
+
+	@PostMapping
+	public ResponseEntity create(@RequestBody Article article, UriComponentsBuilder builder)
+			throws URISyntaxException, InterruptedException, ExecutionException, TimeoutException {
+
+		System.out.println("article = " + article);
+
+		article.setUuid(UUID.randomUUID().toString());
+
+		Operation op = new Operation("article", "create", mapper.valueToTree(article));
+
+		kafka.send(new ProducerRecord<>("shop", article.getUuid(), op)).get(100, TimeUnit.MILLISECONDS);
+
+		//articleRepository.saveAndFlush(article);
+
+		return ResponseEntity.created(builder.path("/articles/" + article.getUuid()).build().toUri()).build();
+	}
+
+	@PatchMapping("/{uuid}")
+	public void update(@PathVariable String uuid, @RequestBody Article neu) throws Exception {
+
+		Article article = articleRepository.findOne(uuid);
+
+		if (article == null) {
+			throw new NotFoundException();
+		}
+
+		Article updateBo = new Article();
+		updateBo.setPrice(neu.getPrice());
+		updateBo.setName(neu.getName());
+		updateBo.setUuid(uuid);
+
+		Operation op = new Operation("article", "update", mapper.valueToTree(updateBo));
+
+		op.logSend();
+
+		kafka.send("shop", op).get(100, TimeUnit.MILLISECONDS);
+	}
+
+	@DeleteMapping("/{uuid}")
+	public void delete(@PathVariable String uuid) throws Exception {
+
+		Article article = articleRepository.findOne(uuid);
+
+		if (article == null) {
+			throw new NotFoundException();
+		}
+
+		Operation op = new Operation("article", "delete", mapper.valueToTree(article));
+
+		op.logSend();
+
+		kafka.send("shop", op).get(100, TimeUnit.MILLISECONDS);
+	}
+
 
 	@GetMapping("/count")
 	public long count() {
--- src/main/java/de/predi8/workshop/catalogue/event/ShopListener.java
+++ src/main/java/de/predi8/workshop/catalogue/event/ShopListener.java
@@ -22,7 +22,44 @@ public class ShopListener {
 	}
 
 	@KafkaListener(topics = "shop")
-	public void listen(Operation op) {
+	public void listen(Operation op) throws InvocationTargetException, IllegalAccessException {
+
+		if (!op.getBo().equals("article")) {
+			return;
+		}
+
+		op.logReceive();
+
 		System.out.println(op);
+
+		Article newArticle = mapper.convertValue(op.getObject(), Article.class);
+		if (newArticle == null) {
+			return;
+		}
+		switch (op.getAction()) {
+
+			case "create":
+				articleRepository.saveAndFlush(newArticle);
+				break;
+
+			case "update":
+				Article article = articleRepository.findOne(newArticle.getUuid());
+				/*
+				if (newArticle.getName() != null) {
+					article.setName(newArticle.getName());
+				}
+				if (newArticle.getPrice() != null) {
+					article.setPrice(newArticle.getPrice());
+				}
+				*/
+				beanUtils.copyProperties(article, newArticle);
+
+				articleRepository.saveAndFlush(article);
+				break;
+
+			case "delete":
+				articleRepository.delete(newArticle.getUuid());
+				break;
+		}
 	}
 }
